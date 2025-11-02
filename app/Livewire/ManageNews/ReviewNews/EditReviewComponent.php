2f70a36125e3e6882c8e55578270f8c5991492ac
Nasrin <ns.kazemi96@gmail.com>
2025-11-02 11:28:37 +0330

    add review text and update

--- app/Livewire/ManageNews/ReviewNews/EditReviewComponent.php
+++ app/Livewire/ManageNews/ReviewNews/EditReviewComponent.php
@@ -33,7 +33,7 @@ class EditReviewComponent extends Component
         ]);
         $newsStep = NewsStep::create([
             'news_id'    => $this->newsId,
-            'step_id'    => 8,
+            'step_id'    => 9,
             'creator_id' => Auth::id(),
         ]);
         News::findOrFail($this->newsId)->update(['status' => $newsStep->id]);
@@ -43,7 +43,7 @@ class EditReviewComponent extends Component
                 'news_id' => $this->newsId,
                 'edited_content' => $this->edited_content,
                 'editor_id'      => Auth::id(),
-                'status'         => 'waiting',
+                'status'         => 'progressing',
             ]
         );
         $this->dispatch('$_review_refresh');
