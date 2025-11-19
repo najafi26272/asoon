090b1a50dd840e5f3df30ef1d7207a4c0d2e0001
Nasrin <ns.kazemi96@gmail.com>
2025-11-19 14:48:52 +0330

    add filter to my review list

--- app/Livewire/ManageNews/ReviewNews/ReviewListComponent.php
+++ app/Livewire/ManageNews/ReviewNews/ReviewListComponent.php
@@ -5,6 +5,8 @@ use Livewire\Component;
 use App\Models\News;
 use Livewire\WithPagination;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ReviewListComponent extends Component
 {
@@ -13,18 +15,14 @@ class ReviewListComponent extends Component
     public $char = "";
     public $news_id, $edited_content, $status, $description;
     public $pageNumber = 10;
+    public $selectedStatus ='all';
+
     protected $listeners = [
         '$_review_refresh' => 'refresh'
     ];
     public function refresh()
     {
-        $searchTerm = '%'.$this->char.'%';
-        $editorId = Auth::user()->id;
-        $items = News::with('editNews')->where('title','LIKE',$searchTerm)->whereHas('editorsAssignments', function ($query) use ($editorId) {
-            $query->where('editor_id', $editorId);
-        })->with(['assignedEditors' => function ($q) use ($editorId) {
-            $q->where('editor_id', $editorId);
-        }])->latest()->paginate($this->pageNumber);
+        $items = $this->getItems();
     }
     public function delete($id)
     {
@@ -44,15 +42,37 @@ class ReviewListComponent extends Component
         $this->dispatch('$_my_review_history', $id);
         $this->dispatch('show-my-review-history-modal');
     }
+
+
+    private function buildQuery(): LengthAwarePaginator
+    {
+        $searchTerm = '%' . $this->char . '%';
+        $editorId = Auth::user()->id;
+        $selectedStatus = $this->selectedStatus;
+        // Start building the query
+        $query = News::with(['editNews','step.stepDefinition'])
+            ->where('title', 'LIKE', $searchTerm)
+            ->whereHas('editorsAssignments', function ($query) use ($editorId) {
+                $query->where('editor_id', $editorId);
+            })
+            ->with(['assignedEditors' => function ($q) use ($editorId) {
+                $q->where('editor_id', $editorId);
+            }]);
+
+        // Apply selected status conditionally
+        if ($this->selectedStatus && $this->selectedStatus !== 'all') {
+            $query->whereHas('step.stepDefinition', function (Builder $q2) use ($selectedStatus) {
+                $q2->where('step_id', $selectedStatus);
+            });
+        }
+
+        return $query->latest()->paginate($this->pageNumber);
+    }
+
     public function render()
     {
-        $searchTerm = '%'.$this->char.'%';
-        $editorId = Auth::user()->id;
-        $items = News::with('editNews')->where('title','LIKE',$searchTerm)->whereHas('editorsAssignments', function ($query) use ($editorId) {
-            $query->where('editor_id', $editorId);
-        })->with(['assignedEditors' => function ($q) use ($editorId) {
-            $q->where('editor_id', $editorId);
-        }])->latest()->paginate($this->pageNumber);
+        $items = $this->buildQuery();
+
         return view('livewire.manage-news.review-news.review-list-component', [
             'items' => $items,
         ]);
